e98200b11c85585d2523be4b188fec82e25ac1f5
RajathInuganti <rajathinuganti@gmail.com>
2025-01-14 06:48:57 -0500

    adding Abstract syntax tree structs

---
+++ ast.h
@@ -0,0 +1,14 @@
+//
+// Created by Rajath Inuganti on 2025-01-04.
+//
+
+#ifndef AST_H
+#define AST_H
+
+#include <stddef.h>
+
+typedef struct Node Node;
+
+Node *newNode(int data);
+
+#endif //AST_H
